6a286e1d331e85c34630d2067a38e89138ab9259
LatellaL <83371530+LatellaL@users.noreply.github.com>
2022-04-24 11:54:53 -0400

    commit tree af7a098afc100b3e079caa95e6fd4a330f03a25f

--- src/GraceHopperQuoteComponent.js
+++ src/GraceHopperQuoteComponent.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 
 export default class GraceHopperQuoteComponent extends Component {
-
 	render() {
 		return (
 			<div className="bar" id="hopper">
-				<i>
+
+<i>
 					<p>
 						&quot;The most dangerous phrase in the language is, ‘We’ve always done it this way.’&quot;
 					</p>
